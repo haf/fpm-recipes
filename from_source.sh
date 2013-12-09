0c328607e2440417d4bba1a31f7e92776b612d3a
Henrik <henrik@haf.se>
2013-12-09 01:28:27 +0100

    adding logstash

---
+++ from_source.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/logstash/logstash.git
+cd logstash
+make flatjar
+
---
+++ recipe.rb
@@ -0,0 +1,27 @@
+class Logstash < FPM::Cookery::Recipe
+  homepage    'http://logstash.net/'
+  name        'logstash'
+  # 1.2.2 fixes:
+  # https://logstash.jira.com/browse/LOGSTASH-1585?jql=project%20%3D%20LOGSTASH%20AND%20affectedVersion%20%3D%20%221.2.2%22%20AND%20status%20in%20%28Resolved%2C%20Closed%29
+  version     '1.2.2'
+  source      "https://download.elasticsearch.org/logstash/logstash/#{name}-#{version}-flatjar.jar"
+  sha1        '5b2dc3e0a09384ea8ed346d55806b3fce2bae5cf'
+
+  revision    '1'
+  maintainer  'Henrik Feldt <henrik@haf.se>'
+  license     'Apache 2'
+  description 'logstash is a tool for managing events and logs. You can use it to collect logs, parse them, and store them for later use (like, for searching).'
+  arch        'all'
+  section     'admin'
+
+  config_files '/etc/logstash/logstash.conf'
+
+  def build
+  end
+
+  def install
+    etc('logstash').install workdir('logstash.conf.example'), 'logstash.conf'
+    opt('logstash').install "#{name}-#{version}-flatjar.jar", 'logstash.jar'
+  end
+end
+
