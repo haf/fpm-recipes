6c757caed4f83224a709113662b86c3089beefab
Henrik <henrik@haf.se>
2013-12-10 11:43:04 +0100

    upgrading elasticsearch

--- elasticsearch/recipe.sh
+++ elasticsearch/recipe.sh
@@ -1,6 +1,6 @@
 #/bin/bash
-sha1=76836f2cc03ced37a141b006e581b03dc4d84dda
-version=0.90.6
+sha1=46c056dca238adc8df2a02e77fc4329b3ececdb5
+version=0.90.7
 filename=elasticsearch-$version.noarch.rpm
 
 if [[ ! -d pkg ]]; then
