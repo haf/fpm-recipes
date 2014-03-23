4096492196429452b0428548985f93b6a54e7c9d
Henrik <henrik@haf.se>
2014-03-23 20:00:52 +0100

    logstash v1.4.0

--- logstash/recipe.rb
+++ logstash/recipe.rb
@@ -3,9 +3,9 @@ class Logstash < FPM::Cookery::Recipe
   name        'logstash'
   # 1.2.2 fixes:
   # https://logstash.jira.com/browse/LOGSTASH-1585?jql=project%20%3D%20LOGSTASH%20AND%20affectedVersion%20%3D%20%221.2.2%22%20AND%20status%20in%20%28Resolved%2C%20Closed%29
-  version     '1.3.2'
-  source      "https://download.elasticsearch.org/logstash/logstash/#{name}-#{version}-flatjar.jar"
-  sha1        'd49d48e0a9590eccb3b8acaa368c01f18125f33d'
+  version     '1.4.0'
+  source      "https://download.elasticsearch.org/logstash/logstash/#{name}-#{version}.tar.gz"
+  sha1        '009c9d3d17b781b6ad2cceb776064cda6c6b3957'
 
   revision    '1'
   maintainer  'Henrik Feldt <henrik@haf.se>'
