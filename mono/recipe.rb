538a0d7351eb47482dc184c2c6e8cb1cac139a75
Henrik Feldt <henrik@haf.se>
2015-01-15 16:52:59 +0100

    mono v3.12.0

--- mono/recipe.rb
+++ mono/recipe.rb
@@ -4,16 +4,16 @@ class Mono < FPM::Cookery::Recipe
   homepage   'http://www.mono-project.com'
 
   name       'mono'
-  version    '3.6.0'
+  version    '3.12.0'
   revision   1
   arch       'x86_64'
   section    'runtimes'
 
   source     "https://github.com/mono/mono.git",
     :with => 'git',
-    :tag  => 'mono-3.6.0.39',
+    :tag  => 'mono-3.12.0.68',
     :submodule => true
-  
+
   maintainer 'Henrik Feldt <henrik@haf.se>'
 
   depends    'libgdiplus'
